a88189549fcf32c52e3532b7b451c1cb3896c32a
dubisr <robert.dubisz@dell.com>
2016-11-29 10:43:31 +0000

    US46055 - CorrelationId builder

---
+++ common-rabbitmq/src/main/java/com/dell/cpsd/common/rabbitmq/utils/CorrelationIdBuilder.java
@@ -0,0 +1,164 @@
+/*
+ * &copy; 2016 VCE Company, LLC. All rights reserved.
+ * VCE Confidential/Proprietary Information
+ */
+
+package com.dell.cpsd.common.rabbitmq.utils;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+/**
+ * Correlation ID builder.
+ * <p>
+ * &copy; 2016 VCE Company, LLC. All rights reserved.
+ * VCE Confidential/Proprietary Information
+ * </p>
+ *
+ * @since SINCE-TBD
+ */
+public class CorrelationIdBuilder
+{
+    /**
+     * Sections separator character.
+     */
+    public static final String SEPARATOR = "$";
+
+    private List<String> sections = new ArrayList<>();
+
+    /**
+     * Default constructor creates an initial random UUID as the first section.
+     */
+    public CorrelationIdBuilder()
+    {
+        sections.add(UUID.randomUUID().toString());
+    }
+
+    /**
+     * Constructor with initial correlationId value.
+     *
+     * @param correlationId previous chain of IDs
+     */
+    public CorrelationIdBuilder(final String correlationId)
+    {
+        this.sections = new ArrayList<>(splitString(correlationId));
+    }
+
+    /**
+     * Add another random UUID section to the existing CorrelationID.
+     *
+     * @return Returns this
+     */
+    public CorrelationIdBuilder addSection()
+    {
+        sections.add(UUID.randomUUID().toString());
+        return this;
+    }
+
+    /**
+     * Add custom value section to the existing CorrelationID.
+     *
+     * @return Returns this
+     */
+    public CorrelationIdBuilder addSection(final String sectionValue)
+    {
+        sections.add(sectionValue);
+        return this;
+    }
+
+    /**
+     * Add another random UUID section to the existing CorrelationID.
+     *
+     * @return Returns this
+     */
+    public String readLast()
+    {
+        if (sections != null && !sections.isEmpty())
+        {
+            return sections.get(sections.size() - 1);
+        }
+        else
+        {
+            return "";
+        }
+    }
+
+    /**
+     * Remove the last section.
+     *
+     * @return Returns this
+     */
+    public CorrelationIdBuilder removeLast()
+    {
+        if (sections != null && !sections.isEmpty())
+        {
+            sections.remove(sections.size() - 1);
+        }
+        return this;
+    }
+
+    public String build()
+    {
+        if (sections != null && !sections.isEmpty())
+        {
+            StringBuilder bld = new StringBuilder(sections.get(0));
+            for (int i = 1; i < sections.size(); i++)
+            {
+                final String section = sections.get(i);
+                bld.append(SEPARATOR);
+                bld.append(section);
+            }
+            return bld.toString();
+        }
+        else
+        {
+            return "";
+        }
+    }
+
+    @Override
+    public String toString()
+    {
+        return build();
+    }
+
+    private List<String> splitString(final String correlationString)
+    {
+        if (StringUtils.isNotEmpty(correlationString))
+        {
+            final String[] array = correlationString.split(SEPARATOR);
+            return Arrays.asList(array);
+        }
+        else
+        {
+            return Collections.emptyList();
+        }
+    }
+
+    @Override
+    public boolean equals(final Object o)
+    {
+        if (this == o)
+        {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass())
+        {
+            return false;
+        }
+        final CorrelationIdBuilder that = (CorrelationIdBuilder) o;
+        return Objects.equals(this.build(), that.build());
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(this.build());
+    }
+}
---
+++ common-rabbitmq/src/test/java/com/dell/cpsd/common/rabbitmq/utils/CorrelationIdBuilderTest.java
@@ -0,0 +1,106 @@
+/*
+ * &copy; 2016 VCE Company, LLC. All rights reserved.
+ * VCE Confidential/Proprietary Information
+ */
+
+package com.dell.cpsd.common.rabbitmq.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Correlation ID builder test.
+ * <p>
+ * &copy; 2016 VCE Company, LLC. All rights reserved.
+ * VCE Confidential/Proprietary Information
+ * </p>
+ *
+ * @since SINCE-TBD
+ */
+public class CorrelationIdBuilderTest
+{
+    @Test
+    public void addSection() throws Exception
+    {
+        CorrelationIdBuilder bld = new CorrelationIdBuilder();
+        bld.addSection();
+        bld.addSection();
+        bld.addSection();
+        assertEquals(36 * 4 + 3, bld.build().length());
+    }
+
+    @Test
+    public void addSectionCustom() throws Exception
+    {
+        CorrelationIdBuilder bld = new CorrelationIdBuilder("a1");
+        bld.addSection("b1");
+        bld.addSection("c1");
+        bld.addSection("d1");
+        assertEquals("a1$b1$c1$d1", bld.build());
+    }
+
+    @Test
+    public void readLast() throws Exception
+    {
+        CorrelationIdBuilder bld = new CorrelationIdBuilder("a1");
+        bld.addSection("b1").addSection("c1").addSection("d1");
+        assertEquals("d1", bld.readLast());
+        assertEquals("c1", bld.removeLast().readLast());
+        assertEquals("a1$b1$c1", bld.build());
+    }
+
+    @Test
+    public void removeLast() throws Exception
+    {
+        CorrelationIdBuilder bld = new CorrelationIdBuilder("a1");
+        bld.addSection("b1").addSection("c1").addSection("d1");
+        assertEquals("a1", bld.removeLast().removeLast().removeLast().readLast());
+        assertEquals("a1", bld.build());
+    }
+
+    @Test
+    public void buildEmpty() throws Exception
+    {
+        CorrelationIdBuilder bld = new CorrelationIdBuilder("");
+        assertEquals("", bld.build());
+
+        bld = new CorrelationIdBuilder(null);
+        assertEquals("", bld.build());
+    }
+
+    @Test
+    public void equals() throws Exception
+    {
+        {
+            CorrelationIdBuilder bld1 = new CorrelationIdBuilder("");
+            CorrelationIdBuilder bld2 = new CorrelationIdBuilder("");
+            assertTrue(bld1.equals(bld2));
+            assertTrue(bld2.equals(bld1));
+        }
+
+        {
+            CorrelationIdBuilder bld1 = new CorrelationIdBuilder("a1");
+            CorrelationIdBuilder bld2 = new CorrelationIdBuilder("a1");
+            assertTrue(bld1.equals(bld2));
+            assertTrue(bld2.equals(bld1));
+        }
+
+        {
+            CorrelationIdBuilder bld1 = new CorrelationIdBuilder("a1$b2$c3");
+            CorrelationIdBuilder bld2 = new CorrelationIdBuilder("a1$b2$c3");
+            assertTrue(bld1.equals(bld2));
+            assertTrue(bld2.equals(bld1));
+        }
+
+        {
+            CorrelationIdBuilder bld1 = new CorrelationIdBuilder("a1$b2$c3");
+            CorrelationIdBuilder bld2 = new CorrelationIdBuilder("a1$b2");
+            assertFalse(bld1.equals(bld2));
+            assertFalse(bld2.equals(bld1));
+        }
+    }
+
+}
