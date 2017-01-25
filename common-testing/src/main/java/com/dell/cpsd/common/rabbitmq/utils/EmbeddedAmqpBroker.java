5eec0a902afd70b7dd14e52944f9aac0cc2b4a59
dubisr <robert.dubisz@dell.com>
2017-01-25 17:29:22 +0000

    US47985 - AMQP tests

---
+++ common-testing/src/main/java/com/dell/cpsd/common/rabbitmq/utils/EmbeddedAmqpBroker.java
@@ -0,0 +1,134 @@
+/*
+ * &copy; 2017 VCE Company, LLC. All rights reserved.
+ * VCE Confidential/Proprietary Information
+ */
+
+package com.dell.cpsd.common.rabbitmq.utils;
+
+import org.apache.qpid.server.Broker;
+import org.apache.qpid.server.BrokerOptions;
+import org.apache.qpid.server.model.VirtualHost;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Closeable;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.springframework.util.SocketUtils.findAvailableTcpPort;
+
+/**
+ * Embedded AMQP message broker for integration tests.<br/>
+ * Uses Apache Qpid.
+ * <p>
+ * Copyright &copy; 2017 Dell Inc. or its subsidiaries. All Rights Reserved.
+ * </p>
+ *
+ * @see <a href="https://dzone.com/articles/mocking-rabbitmq-for-integration-tests">Article the solution is based on</a>
+ * @see <a href="https://qpid.apache.org/">Qpid</a>
+ * @since 1.2
+ */
+public class EmbeddedAmqpBroker implements Closeable
+{
+    private static final Logger LOGGER           = LoggerFactory.getLogger(MessageLoader.class);
+    public static final  String FILE_PASS        = "passwd.properties";
+    public static final  String FILE_QPID_CONFIG = "embedded-config.json";
+
+    private static final int           PORT    = findAvailableTcpPort();
+    private static final BrokerOptions OPTIONS = new BrokerOptions();
+    private static final Broker        BROKER  = new Broker();
+
+    private static EmbeddedAmqpBroker instance;
+
+    private EmbeddedAmqpBroker() throws Exception
+    {
+        init();
+        start();
+    }
+
+    private void init() throws IOException, URISyntaxException
+    {
+        LOGGER.info("Initializing embedded broker");
+        OPTIONS.setConfigProperty("qpid.amqp_port", String.valueOf(PORT));
+        OPTIONS.setConfigProperty("qpid.pass_file", findResourceFilePath(FILE_PASS));
+        final Path tmpPath = Files.createTempDirectory("embeddedBroker");
+        OPTIONS.setConfigProperty("qpid.work_dir", tmpPath.toAbsolutePath().toString());
+        OPTIONS.setConfigProperty("qpid.amqp.version", "0-9"); //Rabbit uses 0.9 AMQP protocol version
+        OPTIONS.setInitialConfigurationLocation(findResourceUrlPath(FILE_QPID_CONFIG));
+    }
+
+    private void start() throws Exception
+    {
+        LOGGER.info("Starting embedded broker. Work-dir: " + OPTIONS.getConfigProperties().get("qpid.work_dir") + " / Port: " + PORT);
+        BROKER.startup(OPTIONS);
+    }
+
+    private String findResourceUrlPath(final String fileName) throws IOException
+    {
+        final URL url = EmbeddedAmqpBroker.class.getResource("/qpid/" + fileName);
+        final String path = url.toExternalForm();
+        return path;
+    }
+
+    private String findResourceFilePath(final String fileName) throws IOException, URISyntaxException
+    {
+        final URL url = EmbeddedAmqpBroker.class.getResource("/qpid/" + fileName);
+        final File file = new File(url.toURI());
+        return file.getAbsolutePath();
+    }
+
+    public Broker getQpidBroker()
+    {
+        return BROKER;
+    }
+
+    public int getPort()
+    {
+        return PORT;
+    }
+
+    /**
+     * Gets default virtual host, should be the only one.
+     */
+    public VirtualHost getVHost()
+    {
+        return BROKER.getBroker().findVirtualHostByName("default");
+    }
+
+    @Override
+    public void close() throws IOException
+    {
+        LOGGER.info("Closing embedded broker");
+        BROKER.shutdown();
+    }
+
+    @Override
+    protected void finalize() throws Throwable
+    {
+        close();
+        super.finalize();
+    }
+
+    /**
+     * Should be changed to Spring bean.
+     */
+    public static EmbeddedAmqpBroker getInstance() throws Exception
+    {
+        if (instance == null)
+        {
+            synchronized (EmbeddedAmqpBroker.class)
+            {
+                if (instance == null)
+                {
+                    instance = new EmbeddedAmqpBroker();
+                }
+            }
+        }
+        return instance;
+
+    }
+}
---
+++ common-testing/src/test/java/com/dell/cpsd/common/rabbitmq/utils/EmbeddedAmqpBrokerTest.java
@@ -0,0 +1,103 @@
+/*
+ * &copy; 2017 VCE Company, LLC. All rights reserved.
+ * VCE Confidential/Proprietary Information
+ */
+
+package com.dell.cpsd.common.rabbitmq.utils;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+import java.util.Properties;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Test the embedded broker.
+ * <p>
+ * Copyright &copy; 2017 Dell Inc. or its subsidiaries. All Rights Reserved.
+ * </p>
+ */
+public class EmbeddedAmqpBrokerTest
+{
+    public static final String PING_EXCHANGE   = "pingExchange";
+    public static final String PING_QUEUE      = "pingQueue";
+    public static final String ROUTING_KEY_ALL = "#";
+
+    private static EmbeddedAmqpBroker embedded;
+
+    @Test
+    public void testEmbeddedBrokerStarts() throws Exception
+    {
+        assertNotNull(embedded.getQpidBroker());
+        assertNotNull(embedded.getQpidBroker().getBroker());
+    }
+
+    @Test
+    public void testCreateExchange() throws Exception
+    {
+        createPingExchangeAndQueue();
+        final CachingConnectionFactory connFactory = new CachingConnectionFactory(embedded.getPort());
+        final RabbitAdmin admin = new RabbitAdmin(connFactory);
+        final Properties queueProperties = admin.getQueueProperties(PING_QUEUE);
+        assertEquals(PING_QUEUE, queueProperties.get("QUEUE_NAME"));
+    }
+
+    @Test
+    public void testSendPing() throws Exception
+    {
+        createPingExchangeAndQueue();
+        final CachingConnectionFactory connFactory = new CachingConnectionFactory(embedded.getPort());
+        final RabbitAdmin admin = new RabbitAdmin(connFactory);
+
+        Properties queueProperties = admin.getQueueProperties(PING_QUEUE);
+        assertEquals(0, queueProperties.get("QUEUE_MESSAGE_COUNT"));
+        sendPing();
+        queueProperties = admin.getQueueProperties(PING_QUEUE);
+        assertEquals(1, queueProperties.get("QUEUE_MESSAGE_COUNT"));
+    }
+
+    private void createPingExchangeAndQueue()
+    {
+        final CachingConnectionFactory connFactory = new CachingConnectionFactory(embedded.getPort());
+        final RabbitAdmin admin = new RabbitAdmin(connFactory);
+        final Queue queue = new Queue(PING_QUEUE, false, false, true);
+        admin.declareQueue(queue);
+        final TopicExchange exchange = new TopicExchange(PING_EXCHANGE);
+        admin.declareExchange(exchange);
+        admin.declareBinding(BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY_ALL));
+        connFactory.destroy();
+    }
+
+    public void sendPing() throws Exception
+    {
+        final CachingConnectionFactory cf = new CachingConnectionFactory(embedded.getPort());
+        final RabbitTemplate template = new RabbitTemplate(cf);
+        final String message = "Ping";
+        template.convertAndSend(PING_EXCHANGE, ROUTING_KEY_ALL, message);
+        cf.destroy();
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception
+    {
+        embedded = EmbeddedAmqpBroker.getInstance();
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception
+    {
+        if (embedded != null)
+        {
+            embedded.close();
+        }
+    }
+}
