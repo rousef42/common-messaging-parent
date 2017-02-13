9b0481773c5cc47f53b080ca1d559fa1f161f8f9
dubisr <robert.dubisz@dell.com>
2017-02-13 01:12:30 +0000

    DE11687 - sysdef-client to RCM api communication update - improved JPA test

--- common-testing/src/main/java/com/dell/cpsd/common/persistence/utils/H2Util.java
+++ common-testing/src/main/java/com/dell/cpsd/common/persistence/utils/H2Util.java
@@ -5,14 +5,11 @@
 
 package com.dell.cpsd.common.persistence.utils;
 
-import com.dell.cpsd.common.rabbitmq.utils.MessageLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.persistence.EntityManager;
 import java.util.List;
-
-import static org.junit.Assert.fail;
 
 /**
  * H2 database helper for tests.
@@ -22,29 +19,25 @@ import static org.junit.Assert.fail;
  *
  * @since 1.2
  */
-public class H2Util
+public class H2Util extends DbTestUtil
 {
-    private static final Logger LOGGER           = LoggerFactory.getLogger(MessageLoader.class);
-    private static final String Q_INTEGRITY_OFF  = "SET REFERENTIAL_INTEGRITY FALSE;";
-    private static final String Q_INTEGRITY_ON   = "SET REFERENTIAL_INTEGRITY TRUE;";
-    private static final String Q_SHOW_TABLES    = "SHOW TABLES;";
-    private static final String Q_TRUNCATE_START = "DELETE FROM ";
-
-    private final EntityManager em;
+    private static final Logger LOGGER          = LoggerFactory.getLogger(H2Util.class);
+    private static final String Q_INTEGRITY_OFF = "SET REFERENTIAL_INTEGRITY FALSE;";
+    private static final String Q_INTEGRITY_ON  = "SET REFERENTIAL_INTEGRITY TRUE;";
+    private static final String Q_SHOW_TABLES   = "SHOW TABLES;";
+    private static final String Q_TRUNCATE      = "DELETE FROM \"%s\";";
 
     public H2Util(final EntityManager em)
     {
-        if (em == null)
-        {
-            fail("EntityManager is null");
-        }
-        this.em = em;
+        super(em);
     }
 
+    @Override
     public void cleanDb()
     {
         LOGGER.info("Cleaning all tables");
         em.getTransaction().begin();
+        LOGGER.info(Q_INTEGRITY_OFF);
         em.createNativeQuery(Q_INTEGRITY_OFF).executeUpdate();
         List tableList = em.createNativeQuery(Q_SHOW_TABLES).getResultList();
         for (final Object row : tableList)
@@ -52,10 +45,11 @@ public class H2Util
             Object[] rowArray = (Object[]) row;
             String tableName = (String) rowArray[0];
             //String schema = (String) rowArray[1];
-            String sqlTruncate = Q_TRUNCATE_START + tableName + ";";
+            String sqlTruncate = String.format(Q_TRUNCATE, tableName);
             LOGGER.info(sqlTruncate);
             em.createNativeQuery(sqlTruncate).executeUpdate();
         }
+        LOGGER.info(Q_INTEGRITY_ON);
         em.createNativeQuery(Q_INTEGRITY_ON).executeUpdate();
         em.getTransaction().commit();
     }
