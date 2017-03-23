ed19ba00b52388ed4d3391f8c3b51d75aa32f075
gouldc <connor.goulding@dell.com>
2017-03-23 17:56:49 +0000

    US52362 - Update Rabbit Context Builder, remove exchange information from message meta data

---
+++ common-rabbitmq/src/test/java/com/dell/cpsd/common/rabbitmq/context/builder/MessageMetaDataReaderTest.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright &copy; 2017 Dell Inc. or its subsidiaries.  All Rights Reserved.
+ */
+
+package com.dell.cpsd.common.rabbitmq.context.builder;
+
+/**
+ * @author Connor Goulding
+ */
+public class MessageMetaDataReaderTest
+{
+}
