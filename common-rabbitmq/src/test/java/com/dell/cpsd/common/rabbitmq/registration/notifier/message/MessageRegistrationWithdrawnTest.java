8ffbbe4068b134f990ef1eaa243065e3d999825b
boettj3 <James.Boettcher@dell.com>
2017-07-13 16:34:57 -0400

    add unit tests

--- common-rabbitmq/src/test/java/com/dell/cpsd/common/rabbitmq/registration/notifier/message/MessageRegistrationWithdrawnTest.java
+++ common-rabbitmq/src/test/java/com/dell/cpsd/common/rabbitmq/registration/notifier/message/MessageRegistrationWithdrawnTest.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright &copy; 2017 Dell Inc. or its subsidiaries.  All Rights Reserved.
+ * Dell EMC Confidential/Proprietary Information
+ */
+
 package com.dell.cpsd.common.rabbitmq.registration.notifier.message;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
