90b0f467d8389a9bdfd2e51f3a7790fd8f634139
Karthik Rao <karthik.srinivasrao@dell.com>
2017-08-28 15:54:34 +0530

    Fix Test Case
    Fix Test Case

--- common-rabbitmq/src/test/java/com/dell/cpsd/common/rabbitmq/context/builder/MessageMetaDataReaderTest.java
+++ common-rabbitmq/src/test/java/com/dell/cpsd/common/rabbitmq/context/builder/MessageMetaDataReaderTest.java
@@ -25,7 +25,7 @@ public class MessageMetaDataReaderTest
     {
         MessageMetaDataReader reader = new MessageMetaDataReader();
         Collection<MessageMetaData> metaDatas = reader
-                .read(new File(ClassLoader.getSystemClassLoader().getResource("api-schema/messaging.json").getFile()));
+                .read(new File(ClassLoader.getSystemClassLoader().getResource("./api-schema/messaging.json").getFile()));
         Assert.assertEquals(2, metaDatas.size());
     }
 }
